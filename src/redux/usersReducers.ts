607405ba71a080c1023f73c830791fd8a88ec2bc
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2023-02-12 13:28:03 +0300

    '49UsersAC2'

--- src/redux/usersReducers.ts
+++ src/redux/usersReducers.ts
@@ -1,18 +1,43 @@
-import {ActionTypes, ProfileStateType} from "./store";
-
-type UsersReducerType = (state: any, action: ActionTypes) => ProfileStateType
+import {} from "./store";
+import {addMessageAC, onChangeMessageAC} from "./dialogsReduser";
+import {addPostAC, updateNewPostTextAC} from "./profileReducer";
 
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
+const SET_USERS = 'SET-USERS'
 
 
+/*type FollowActionType = {
+    type: string
+    userId: string
+}
+type UnfollowActionType = {
+    type: string
+    userId: string
+}
+type SetUsersActionType = {
+    type: string,
+    users: UserType[]
+}*/
+// type UserReducerActionTypes = FollowActionType | UnfollowActionType | SetUsersActionType
 
+type UserType = {
+    id: string
+    fullName: string
+    status: string
+    location: {country: string, city: string}
+    avatar: string
+    followed: boolean
+}
+type StateType = {
+    users: UserType[]
+}
+type UsersReducerType = (state: StateType, action: ActionTypes) => StateType
 
-
-let initialState: any = {
+let initialState: StateType  = {
     users: [
         {
-            id: 1,
+            id: '1',
             fullName: 'Dmitry',
             status: 'So tired',
             location: {
@@ -23,7 +48,7 @@ let initialState: any = {
             followed: false
         },
         {
-            id: 2,
+            id: '2',
             fullName: 'Stanislav',
             status: 'Some status',
             location: {
@@ -34,7 +59,7 @@ let initialState: any = {
             followed: false
         },
         {
-            id: 3,
+            id: '3',
             fullName: 'Alexander',
             status: 'Nice day!',
             location: {
@@ -48,14 +73,27 @@ let initialState: any = {
 }
 
 
-const usersReducers: UsersReducerType = (state = initialState, action: any) => {
-    const stateCopy = {...state}
+const usersReducers: UsersReducerType = (state = initialState, action: ActionTypes): StateType => {
+
     switch (action.type) {
         case FOLLOW: {
-            return stateCopy
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    return u.id === action.userId ? {...u, followed: true} : u
+                })
+            }
         }
         case UNFOLLOW: {
-            return stateCopy
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    return u.id === action.userId ? {...u, followed: false} : u
+                })
+            }
+        }
+        case SET_USERS: {
+            return {...state, users: [...state.users, ...action.users]}
         }
         default: {
             return state
@@ -63,11 +101,18 @@ const usersReducers: UsersReducerType = (state = initialState, action: any) => {
     }
 }
 
-const followAC = () => {
-    return { type: FOLLOW }
+const followAC = (userId: string) => {
+    return { type: FOLLOW, userId } as const
 }
-const unfollowAC = () => {
-    return { type: UNFOLLOW }
+const unfollowAC = (userId: string) => {
+    return { type: UNFOLLOW, userId } as const
 }
+const setUsersAC = (users: UserType[]) => {  // Тут под вопросом в каком виде приходят users, пока UserType[]
+    return {type: SET_USERS, users} as const
+}
+
+export type ActionTypes = ReturnType<typeof followAC> |
+    ReturnType<typeof unfollowAC> |
+    ReturnType<typeof setUsersAC>
 
 export default usersReducers;
